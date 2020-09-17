a2759cc6c2f2ffd2447edf1e2976da1c8bd8c825
messyv <71394934+messyv@users.noreply.github.com>
2020-09-17 18:51:39 +0200

    Add files via upload

---
+++ terulet.py
@@ -0,0 +1,8 @@
+num1 = input ("Enter a:")
+num2 = input ("Enter b:")
+num3 = input ("Enter c:")
+num4 = input ("Enter d:")
+result = (float(num1) * float(num2))*2
+result2 = float(num3) * float(num4)
+result3 = result + result2
+print (result3)
